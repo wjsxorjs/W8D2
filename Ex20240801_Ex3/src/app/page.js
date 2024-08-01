54c991aa7dd8b7defada50158c727d937f77ccaa
wjsxorjs <wjsxorjs@naver.com>
2024-08-01 14:47:13 +0900

    20240801 NextJS 0.6

--- Ex20240801_Ex3/src/app/page.js
+++ Ex20240801_Ex3/src/app/page.js
@@ -1,11 +1,41 @@
+"use client"
+
+import { Grid } from "@mui/material";
+import axios from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 // import styles from "./page.module.css"; // 특정한 객체만 사용할 수 있음
 
 export default function Home() {
-  const api_url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const api_url = "/api/v1/products.json?brand=maybelline";
+
+  const [list, setList] = useState([]);
+
+
+  const getData = () => {
+    axios.get(
+      api_url
+    ).then(data => {
+      // console.log(data);
+      setList(data.data);
+    })
+  };
+
+  // 최초 한 번만 호출하기 위해 사용함!
+  useEffect(() => {
+    getData();
+  },[]);
+
   return (
     <main style={{padding: '20px', width: '80%', margin: 'auto'}}>
-      
+      <Grid container>
+        {list.map((item)=>(
+          <Grid item key={item.id} xs={3}>
+            <img src={item.image_link} alt={item.name} title={item.name}/>
+            <div>{item.name}</div>
+          </Grid>
+        ))}
+      </Grid>
     </main>
   );
 }
